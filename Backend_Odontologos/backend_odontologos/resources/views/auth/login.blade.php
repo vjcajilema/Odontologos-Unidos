1b89e04868cc747f1dca82abd120ce49e3a8a95e
vjcajilema <vjcajilema@espe.edu.ec>
2020-07-09 06:16:37 -0500

    correcciones

--- Backend_Odontologos/backend_odontologos/resources/views/auth/login.blade.php
+++ Backend_Odontologos/backend_odontologos/resources/views/auth/login.blade.php
@@ -8,24 +8,14 @@
             <div class="col-lg-5 col-md-7">
                 <div class="card bg-secondary shadow border-0">
                     <div class="card-header bg-transparent pb-5">
-                        <div class="text-muted text-center mt-2 mb-3"><small>{{ __('Sign in with') }}</small></div>
-                        <div class="btn-wrapper text-center">
-                            <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/github.svg"></span>
-                                <span class="btn-inner--text">{{ __('Github') }}</span>
-                            </a>
-                            <a href="#" class="btn btn-neutral btn-icon">
-                                <span class="btn-inner--icon"><img src="{{ asset('argon') }}/img/icons/common/google.svg"></span>
-                                <span class="btn-inner--text">{{ __('Google') }}</span>
-                            </a>
-                        </div>
+
                     </div>
                     <div class="card-body px-lg-5 py-lg-5">
                         <div class="text-center text-muted mb-4">
                             <small>
-                                    Create new account OR Sign in with these credentials:
+                                    Ingrese a la aplicacion utilizando su correo electronico y su contraseña
                                     <br>
-                                    Username <strong>admin@argon.com</strong> Password: <strong>secret</strong>
+
                             </small>
                         </div>
                         <form role="form" method="POST" action="{{ route('login') }}">
@@ -60,11 +50,11 @@
                             <div class="custom-control custom-control-alternative custom-checkbox">
                                 <input class="custom-control-input" name="remember" id="customCheckLogin" type="checkbox" {{ old('remember') ? 'checked' : '' }}>
                                 <label class="custom-control-label" for="customCheckLogin">
-                                    <span class="text-muted">{{ __('Remember me') }}</span>
+                                    <span class="text-muted">{{ __('Recuerdame') }}</span>
                                 </label>
                             </div>
                             <div class="text-center">
-                                <button type="submit" class="btn btn-primary my-4">{{ __('Sign in') }}</button>
+                                <button type="submit" class="btn btn-primary my-4">{{ __('Ingresar') }}</button>
                             </div>
                         </form>
                     </div>
@@ -73,13 +63,13 @@
                     <div class="col-6">
                         @if (Route::has('password.request'))
                             <a href="{{ route('password.request') }}" class="text-light">
-                                <small>{{ __('Forgot password?') }}</small>
+                                <small>{{ __('Olvidó su contraseña?') }}</small>
                             </a>
                         @endif
                     </div>
                     <div class="col-6 text-right">
                         <a href="{{ route('register') }}" class="text-light">
-                            <small>{{ __('Create new account') }}</small>
+                            <small>{{ __('Crear una cuenta') }}</small>
                         </a>
                     </div>
                 </div>
